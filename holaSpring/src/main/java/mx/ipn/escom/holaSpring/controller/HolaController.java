a029b50a03e97646a4236d85e8a87be717b99331
Davidayala10 <deiiviid622@gmail.com>
2025-02-25 19:35:04 -0600

    Controller

--- holaSpring/src/main/java/mx/ipn/escom/holaSpring/controller/HolaController.java
+++ holaSpring/src/main/java/mx/ipn/escom/holaSpring/controller/HolaController.java
@@ -1,4 +1,4 @@
-package mx.ipn.escom.holaSpring.test;
+package mx.ipn.escom.holaSpring.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/hola")
-public class Controller {
+public class HolaController {
     
 
     @GetMapping
